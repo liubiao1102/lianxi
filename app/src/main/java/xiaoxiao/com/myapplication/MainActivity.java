6867c63ede1db41b8c9fc2145cd48f955795c89c
lb <1574345796@qq.com>
2019-02-22 18:54:28 +0800

    提交代码

---
+++ app/src/main/java/xiaoxiao/com/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package xiaoxiao.com.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //测试项目
+    }
+}
